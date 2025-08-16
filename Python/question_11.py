f79d85556246594c8ad21ba93415367044a5daf9
Poojasoni1110 <poojasoni9964@gmail.com>
2025-08-16 14:27:57 +0530

    Delete Python/question_11.py

--- Python/question_11.py
+++
@@ -1,9 +0,0 @@
-#Write a Python program to test whether a passed letter is a vowel or not.
- 
-char = input("Enter the character::")
-if len(char)!=1 or not  char.isalpha():
-    print("Please enter the single alphabet and Digit is not valid")
-elif char.lower() in 'aeiou':
-    print(f"{char} is a vowel")
-else:
-    print(f"{char} is not a vowel")
